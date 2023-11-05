a3abcb3db3490cf2133cdbf9402b785b37274608
Sawa-22 <149924851+Sawa-22@users.noreply.github.com>
2023-11-06 08:56:35 +0900

    Update B9member.java

--- B9member.java
+++ B9member.java
@@ -1,5 +1,5 @@
 
 public class B9member {
-	System.out.println(); // DBから各年のベストナインを引っ張ってくきて出力させる
+	System.out.println(); // DBから各年のベストナインを引っ張ってきて出力させる
 	System.out.println( + "年" + "リーグ" + "のベストナイン");
 }
