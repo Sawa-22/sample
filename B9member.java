c4ce60e180af3220ec649a0d92525c04e447b00c
Sawa-22 <149924851+Sawa-22@users.noreply.github.com>
2023-11-06 11:38:51 +0900

    Update B9member.java

--- B9member.java
+++ B9member.java
@@ -1,15 +1,18 @@
 
 public class B9member {
-	public static void main(String []args) {
-	public int year;
-	public String league;
-	public int position;
-	public String name;
-	public String team;
 	
-	System.out.println(); // DBから各年のベストナインを引っ張ってきて出力させる
-	System.out.println( + "年" + "リーグ" + "のベストナイン");
+	public static void main(String []args) {
+		public int year;
+		public String league;
+		public int position;
+		public String name;
+		public String team;
+	
+		System.out.println(); // DBから各年のベストナインを引っ張ってきて出力させる
+		System.out.println( + "年" + "リーグ" + "のベストナイン");
+	}
 }
+
 //データベースに接続するメソッド↓
 //private static String getSqlite3Path(){
 //    return System.getProperty("user.dir") + File.separator + "sql" + File.separator + "juku.sqlite3";
