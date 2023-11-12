8108efb070d25297cc8be65dbb99f8db5704019c
Sawa-22 <149924851+Sawa-22@users.noreply.github.com>
2023-11-12 20:29:27 +0900

    SQLiteからベストナインのデータを表示させたい
    ほぼ写経

---
+++ mybestnine.java
@@ -0,0 +1,78 @@
+import java.io.File;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class mybestnine {
+	
+	int year;
+	String league;
+	String position;
+	String name;
+	String team;
+	
+	// System.out.println(year + "年" + league + "リーグ" + "のベストナイン");
+
+	// コンストラクタ
+	public mybestnine(int year, String league, String position, String name, String team) {
+		this.year = year;
+		this.league = league;
+		this.position = position;
+		this.name = name;
+		this.team = team;
+	}
+	
+	public int getYear() { // yearをgetするメソッド（それぞれ書く）
+		return year;
+	}
+	
+	public String getLeague() {
+		return league;
+	}
+	
+	public String getPosition() {
+		return position;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getTeam() {
+		return team;
+	}
+	
+	// DB接続メソッド
+	private static String getSqlite3Path(){
+		    return System.getProperty("user.dir") + File.separator + "sql" + File.separator + "juku.sqlite3";
+	}
+
+	
+	// テーブル取得メソッド
+	public static List<mybestnine> fetchB9member() {
+		List<mybestnine> rows = new ArrayList<mybestnine>();
+		
+		try {
+			Connection connection = DriverManager.getConnection("jdbc:sqlite://localhost/mybestnine"()); //getSqlite3Path()
+			Statement createStatement = connection.createStatement();
+			ResultSet resultSet = createStatement.executeQuery("select * from mybestnine;"); //getSqlite3Path()
+			
+			while (resultSet.next()) {
+				int year = resultSet.getInt("year");
+				String name = resultSet.getString("league");
+				String position = resultSet.getString("position");
+				String name1 = resultSet.getString("name"); //なぜname1??
+				String team = resultSet.getString("team");
+				
+				mybestnine row = new mybestnine(0, null, null, null, null);
+				rows.add(row);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return rows;
+	}
+}
