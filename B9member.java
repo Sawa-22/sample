9a67ac8d3948e9e8a3077774b437811fe146bdef
Sawa-22 <149924851+Sawa-22@users.noreply.github.com>
2023-11-15 11:06:51 +0900

    Update B9member.java
    DBから引っ張ってくるのは難しかったので、配列を使いたい

--- B9member.java
+++ B9member.java
@@ -1,22 +1,34 @@
+import java.util.Scanner;
 
-public class B9member {
-	public static void main(String[]args) {
-		int[] id = {1, 2, 3, 4, 5, 6, 7, 8, 9}; //　ポジションと被る
-		int[] years = {2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023};
-		String[] league = {"セントラル", "パシフィック"};
-		String[] position = {"P", "C", "1B", "2B", "3B", "SS", "OF1", "OF2", "OF3", "DH"}; // LF, CF, RF
-		String[] name = {"吉見一起", "null", "栗原健太", "平野恵一", "宮本慎也", "鳥谷敬", "マットマートン", "青木宣親", "長野久義"}; //DB管理
-		String[] team = {"Giants", "Tigers", "Dragons", "Swallows", "Carp", "BayStars", "Hawks", "Lions", "Baffaloes", "Fighters", "Marines", "Eagles"};
+public class mybestnine {
+	public static void main(String[] args) {
+	System.out.println("シーズン/リーグを入力してください > ");
 	
-		public static void main(String []args) {
-			// データ表示のためのメインメソッド
-			// System.out.print(year + "年" + league "リーグ" + "ベストナイン");
-			// System.out.println(); // DBから各年のベストナインを引っ張ってきて出力させる、DBを作るためのコマンドラインをあげる？？
-		}
+	Scanner scan = new Scanner(System.in);
+	// 2015～2022の中から知りたいシーズンを選択できるようにする。
+	int year = scan.nextLine();
+	// セ・パの2つなので、選択できるようにする。
+	String league = scan.nextLine();
+	
+	
+	System.out.println(year + "年" + league + "リーグ" + "のベストナイン");
+	scan.close();
 
-		//データベースに接続するメソッド
-		private static String getSqlite3Path(){
-    			return System.getProperty("user.dir") + File.separator + "sql" + File.separator + "juku.sqlite3";
+		int year = 2022;
+		if (year == 2022) { // "セ" or "パ"
+			
+		System.out.println("P " + "青柳晃洋" + "T ");
+		System.out.println("C " + "木下拓哉" + "D ");
+		System.out.println("1B" + "J.オスナ" + "Ys");
+		System.out.println("2B" + "牧秀悟　" + "DB");
+		System.out.println("3B" + "村上宗隆" + "Ys");
+		System.out.println("SS" + "中野拓夢" + "T ");
+		System.out.println("LF" + "佐野恵太" + "DB");
+		System.out.println("CF" + "近本光司" + "T ");
+		System.out.println("RF" + "岡林勇希" + "D ");
 		}
-	}
+		
+		//String position;
+        	//String name;
+		//String team;
 }
